752aabc43d556882ff40b3d2e5f13251ef54f9d5
nahla_epic <nahla.s1995@gmail.com>
2022-02-07 03:07:16 +0200

    Adding utility classes

---
+++ app/src/main/java/com/nahlasamir244/flickrsearchapp/utils/Utils.kt
@@ -0,0 +1,6 @@
+package com.nahlasamir244.flickrsearchapp.utils
+
+//General Utils file
+//can be used to turn statement to expression by returning the object itself
+val <T> T.exhaustive: T
+    get() = this
---
+++ app/src/main/java/com/nahlasamir244/flickrsearchapp/utils/helpers/MessageHelper.kt
@@ -0,0 +1,9 @@
+package com.nahlasamir244.flickrsearchapp.utils.helpers
+
+fun displayToast(){
+
+}
+
+fun displaySnackBar(){
+
+}
