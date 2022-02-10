4beb09cdf09bd3fbfc677a0db5b14a30eb1ab3d8
nahla_epic <nahla.s1995@gmail.com>
2022-02-10 08:45:48 +0200

    adding search service

--- app/src/main/java/com/nahlasamir244/flickrsearchapp/data/api/PhotoApiService.kt
+++ app/src/main/java/com/nahlasamir244/flickrsearchapp/data/api/PhotoApiService.kt
@@ -1,4 +1,20 @@
 package com.nahlasamir244.flickrsearchapp.data.api
 
+import com.nahlasamir244.flickrsearchapp.data.model.photo_search.PhotoSearchResponse
+import com.nahlasamir244.flickrsearchapp.utils.Constants
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface PhotoApiService {
+    @GET(".")
+    suspend fun getPhotoSearchList(
+        @Query("text") searchQuery: String,
+        @Query("page") pageNumber: Int,
+        @Query("per_page") numberOfPhotosPerPage: Int,
+        @Query("method") method: String="flickr.photos.search",
+        @Query("nojsoncallback") noJson: Int=50,
+        @Query("format") format: String=Constants.RESPONSE_FORMAT,
+        @Query("api_key") apiKey: String=Constants.FLICKR_API_KEY,
+        ): Response<PhotoSearchResponse>
 }
